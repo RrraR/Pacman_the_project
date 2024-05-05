5e05ce69d8839163aa5a64c58898569540ac422b
RrraR <tanja.budnitskay@gmail.com>
2024-05-05 14:31:02 +0200

    draw recs instead of buttons

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,52 +1,101 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class MainWindow{
+
     public JFrame frame;
-    public GameFrame2 gameFrame1;
     private JPanel mapPanel;
     private JPanel statusBar;
 
-    MainWindow(){
+    final static int W=1; // Wall.
+    final static int F=2; // Crossroads with food
+    final static int E=3; // Empty crossroads
+    private static int board[][] = {
+            //-----------------------X-----------------------------//
+            {W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W},
+            {W,F,F,F,F,F,F,F,F,F,F,F,F,W,W,F,F,F,F,F,F,F,F,F,F,F,F,W},
+            {W,F,W,W,W,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,W,W,W,F,W},
+            {W,F,W,W,W,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,W,W,W,F,W},
+            {W,F,W,W,W,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,W,W,W,F,W},
+            {W,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,W},
+            {W,F,W,W,W,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,W,W,W,F,W},
+            {W,F,W,W,W,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,W,W,W,F,W},
+            {W,F,F,F,F,F,F,W,W,F,F,F,F,W,W,F,F,F,F,W,W,F,F,F,F,F,F,W},
+            {W,W,W,W,W,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,W,W,W,W,W},
+            {E,E,E,E,E,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,E,E,E,E,E},
+            {E,E,E,E,E,W,F,W,W,F,F,F,F,F,F,F,F,F,F,W,W,F,W,E,E,E,E,E},
+            {E,E,E,E,E,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,E,E,E,E,E},
+            {W,W,W,W,W,W,F,W,W,F,W,E,E,E,E,E,E,W,F,W,W,F,W,W,W,W,W,W},
+            {F,F,F,F,F,F,F,F,F,F,W,E,E,E,E,E,E,W,F,F,F,F,F,F,F,F,F,F},
+            {W,W,W,W,W,W,F,W,W,F,W,E,E,E,E,E,E,W,F,W,W,F,W,W,W,W,W,W},
+            {E,E,E,E,E,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,E,E,E,E,E},
+            {E,E,E,E,E,W,F,W,W,F,F,F,F,F,F,F,F,F,F,W,W,F,W,E,E,E,E,E},
+            {E,E,E,E,E,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,E,E,E,E,E},
+            {W,W,W,W,W,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,W,W,W,W,W},
+            {W,F,F,F,F,F,F,F,F,F,F,F,F,W,W,F,F,F,F,F,F,F,F,F,F,F,F,W},
+            {W,F,W,W,W,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,W,W,W,F,W},
+            {W,F,W,W,W,W,F,W,W,W,W,W,F,W,W,F,W,W,W,W,W,F,W,W,W,W,F,W},
+            {W,F,F,F,W,W,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,W,W,F,F,F,W},
+            {W,W,W,F,W,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,W,F,W,W,W},
+            {W,W,W,F,W,W,F,W,W,F,W,W,W,W,W,W,W,W,F,W,W,F,W,W,F,W,W,W},
+            {W,F,F,F,F,F,F,W,W,F,F,F,F,W,W,F,F,F,F,W,W,F,F,F,F,F,F,W},
+            {W,F,W,W,W,W,W,W,W,W,W,W,F,W,W,F,W,W,W,W,W,W,W,W,W,W,F,W},
+            {W,F,W,W,W,W,W,W,W,W,W,W,F,W,W,F,W,W,W,W,W,W,W,W,W,W,F,W},
+            {W,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,W},
+            {W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W}
+    };
+
+    MainWindow() throws IOException {
         //extra height 35
         //extra width 12
         frame = new JFrame("Main Window Test");
         frame.setSize(431, 529);
-//        frame.setPreferredSize(new Dimension(572, 655));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.setResizable(false);
 
+        createStatusBar();
+        frame.getContentPane().add(statusBar, BorderLayout.NORTH);
+
+        drawBoardBackground();
+        frame.getContentPane().add(mapPanel);
+
+        frame.setVisible(true);
+
+    }
+
+    private void createStatusBar(){
         statusBar = new JPanel();
         statusBar.setPreferredSize(new Dimension(572, 30));
         statusBar.setBackground(Color.black);
-        statusBar.setVisible(true);
-        frame.getContentPane().add(statusBar, BorderLayout.NORTH);
-
-        gameFrame1 = new GameFrame2();
-        gameFrame1.setLayout(null);
-        frame.getContentPane().add(gameFrame1);
-        frame.setVisible(true);
-
-
-//        gameFrame1.setPreferredSize(new Dimension(600,120));
-//        gameFrame1.setBackground(Color.black);
-
-//        gameFrame2 = new GameFrame2();
-//        gameFrame2.setPreferredSize(new Dimension(600,580));
-//        gameFrame2.setBackground(Color.BLACK);
-//        frame.getContentPane().add(gameFrame2, BorderLayout.SOUTH);
-
-    }
-
-    private JPanel createStatusBer(){
-        JPanel statusBar = new JPanel();
-
-        return statusBar;
     }
 
     private void drawBoardBackground(){
+        mapPanel = new JPanel(){
+//            private Image pac = ImageIO.read(getClass().getClassLoader().getResource("img/" + "Heart.png"));
 
+            public void paint (Graphics g){
+                super.paint(g);
+                for (int i = 0; i < board[0].length; i++)
+                {
+                    for (int j = 0; j < board.length; j++)
+                    {
+                        g.setColor(Color.white);
+                        if (board[j][i] == W)
+                        {
+                            g.setColor(Color.BLACK);
+                        }
+                        else if (board[j][i] == F)
+                        {
+                            g.setColor(Color.yellow);
+                        }
+                        g.fillRect(i * 15,j * 15, 15, 15);
+                    }
+                }
+
+            }
+        };
     }
 
 }
